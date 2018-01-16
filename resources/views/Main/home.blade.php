b432b881e2582640b431b35dc20b8645edfccfc4
Ivan Rojo <ivanrojo07@gmail.com>
2018-01-15 22:50:32 -0600

    10:48

--- resources/views/Main/home.blade.php
+++ resources/views/Main/home.blade.php
@@ -14,10 +14,6 @@
 
         <section id="banner">
 
-            
-            <div style="padding-top: -320px !important;">
-                <nav class="navbar navbar-inverse"> <div class="container-fluid"> <div class="navbar-header"> <button type="button" class="collapsed navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-9" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a href="#" class="navbar-brand">Brand</a> </div> <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-9"> <ul class="nav navbar-nav"> <li class="active"><a href="#">Home</a></li> <li><a href="#">Link</a></li> <li><a href="#">Link</a></li> </ul> </div> </div> </nav>
-            </div>    
             <div class="content">
 
                 <header>
