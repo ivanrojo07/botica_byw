428154ab88081690edc7c279acd0eaba19553416
Iyari <iyari.almeida@gmail.com>
2018-02-22 11:09:41 -0600

    stash

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,10 +8,10 @@ class ComposerStaticInitd38d9d698b314519b1e00e3097a63845
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '1d1b89d124cc9cb8219922c9d5569199' => __DIR__ . '/..' . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
-        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
+        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
@@ -101,8 +101,8 @@ class ComposerStaticInitd38d9d698b314519b1e00e3097a63845
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'XdgBaseDir\\' => 
         array (
@@ -332,6 +332,7 @@ class ComposerStaticInitd38d9d698b314519b1e00e3097a63845
         'App\\Http\\Controllers\\ProductsController' => __DIR__ . '/../..' . '/app/Http/Controllers/ProductsController.php',
         'App\\Http\\Controllers\\PromotionsController' => __DIR__ . '/../..' . '/app/Http/Controllers/PromotionsController.php',
         'App\\Http\\Controllers\\ShoppingCartsController' => __DIR__ . '/../..' . '/app/Http/Controllers/ShoppingCartsController.php',
+        'App\\Http\\Controllers\\Tracking\\TrackingController' => __DIR__ . '/../..' . '/app/Http/Controllers/Tracking/TrackingController.php',
         'App\\Http\\Controllers\\UserOrdersController' => __DIR__ . '/../..' . '/app/Http/Controllers/UserOrdersController.php',
         'App\\Http\\Controllers\\userprofileController' => __DIR__ . '/../..' . '/app/Http/Controllers/userprofileController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
@@ -358,6 +359,7 @@ class ComposerStaticInitd38d9d698b314519b1e00e3097a63845
         'App\\Providers\\ShoppingCartProvider' => __DIR__ . '/../..' . '/app/Providers/ShoppingCartProvider.php',
         'App\\Services\\FavoriteProduct' => __DIR__ . '/../..' . '/app/Services/FavoriteProduct.php',
         'App\\ShoppingCart' => __DIR__ . '/../..' . '/app/ShoppingCart.php',
+        'App\\Tracking' => __DIR__ . '/../..' . '/app/Tracking.php',
         'App\\User' => __DIR__ . '/../..' . '/app/User.php',
         'Carbon\\Carbon' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/Carbon.php',
         'Carbon\\CarbonInterval' => __DIR__ . '/..' . '/nesbot/carbon/src/Carbon/CarbonInterval.php',
